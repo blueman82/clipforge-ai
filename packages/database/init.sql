52d12277351d67a6a7cbda3694c3be2dc95acc08
blueman82 <>
2025-09-02 02:11:36 +0100

    Modify init.sql • Tool: Write • Timestamp: 2025-09-02 02:11:36

---
+++ packages/database/init.sql
@@ -0,0 +1,8 @@
+-- ClipForge AI Database Initialization
+-- This file is loaded when the PostgreSQL container starts
+
+-- Create extensions if needed
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- Database is already created by POSTGRES_DB environment variable
+-- User and permissions are set by POSTGRES_USER/POSTGRES_PASSWORD
